7079ad07666b91a3a25aa897a9e814aa1189e029
BTran299 <tranbao299@gmail.com>
2021-02-04 11:07:58 +0700

    init

---
+++ projects/b-angular/src/lib/b-angular.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BAngularComponent } from './b-angular.component';
+
+
+
+@NgModule({
+  declarations: [BAngularComponent],
+  imports: [
+  ],
+  exports: [BAngularComponent]
+})
+export class BAngularModule { }
---
+++ projects/b-angular/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of b-angular
+ */
+
+export * from './lib/b-angular.component';
+export * from './lib/b-angular.module';
